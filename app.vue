587c8ee3955f50b5664dd79dec1fdcf32b506275
Ben Eastwood <83828090+k4ben@users.noreply.github.com>
2024-05-12 01:19:23 -0400

    add dropdown

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="dropdown">
+  <div tabindex="0" role="button" class="btn m-1">Click</div>
+  <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
 </template>
