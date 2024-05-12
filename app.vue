ff87eb2840c7c531b04f3a0405df68bb5dfb3e89
Ben Eastwood <83828090+k4ben@users.noreply.github.com>
2024-05-12 03:04:57 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dropdown">
-  <div tabindex="0" role="button" class="btn m-1">Click</div>
-  <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+  <div data-dropdown-toggle="dropdown" tabindex="0" role="button" class="btn m-1">Click</div>
+  <ul id="dropdown" tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
     <li><a>Item 1</a></li>
     <li><a>Item 2</a></li>
   </ul>
